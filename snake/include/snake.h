6a271d83af8549fa5ae9a7de159fe4d23ad5bd92
Alexandre Garbé <alexandre.garbe@epitech.eu>
2024-02-06 18:47:25 +0100

    edit: edited snake.h to add an include lib

--- snake/include/snake.h
+++ snake/include/snake.h
@@ -1,12 +1,36 @@
 #ifndef SNAKE
     #define SNAKE
+    #include "include.h"
 
-#include <unistd.h>
 
+//STRUCT
+typedef struct s_map
+{
+    int rows;
+    int cols;
+    char **map;
+} t_map;
+
+struct flag_s
+{
+    char *flag;
+    void (*f)(int ac, char **av);
+};
+//FUNC
+char **str_to_wordarray(char *str, char separator)
+//SRC
 void fill_board();
 void print_board();
 void my_putchar(char c);
 int main(int ac, char **av);
 
+static const struct flag_s flags[] = {
+    {"-h", &help},
+    {"--help", &help},
+    {"-f", &map_loader},
+    {"--file", &map_loader},
+    {"-g", &generator},
+    {"--generate", &generator},
+};
 
 #endif /*SNAKE*/
