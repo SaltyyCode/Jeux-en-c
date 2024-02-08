6182ec636b636976ac3d6354e8a3e5151f806aaf
Saltyy <clement.bouret@epitech.eu>
2024-02-08 18:30:49 +0100

    [+] .h for TicTacToe

---
+++ TicTacToe/include/tictactoe.h
@@ -0,0 +1,21 @@
+#ifndef TICTACTOE
+    #define TICTACTOE
+
+#include <stdio.h>
+#include <stdlib.h>
+
+extern char board[3][3];
+extern const char JOUEUR;
+extern const char JOUEUR2;
+extern char WINNER;
+
+void player_move();
+void player_move2();
+void reset_board();
+void print_board();
+int check_cell();
+int check_move();
+void check_win(char WINNER);
+
+
+#endif /*TICTACTOE*/
