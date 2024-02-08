1f0f20e7a0443333cc8c428ab4807f87efc731ab
Saltyy <clement.bouret@epitech.eu>
2024-02-08 18:55:59 +0100

    [~] do while added to handle a new game mechanic

--- TicTacToe/src/main.c
+++ TicTacToe/src/main.c
@@ -5,24 +5,32 @@ const char JOUEUR = 'O';
 const char JOUEUR2 = 'X';
 char WINNER = ' ';
 
-
-int main ()
-{
-    reset_board();
-    while (WINNER == ' ' && check_cell() != 0){
-        print_board();
-        player_move();
-        WINNER = check_move();
-        if (WINNER != ' ' || check_cell() == 0)
-            break;
-        print_board();
-        player_move2();
-        WINNER = check_move();
-        if (WINNER != ' ' || check_cell() == 0)
-            break;
-        print_board();
-    }
-    print_board();
-    check_win(WINNER);
+int main() {
     
+    char continuer = 'O';
+
+    while (continuer == 'O' || continuer == 'o') {
+        reset_board();
+        WINNER = ' ';
+
+
+        while (WINNER == ' ' && check_cell() != 0) {
+            print_board();
+            player_move();
+            WINNER = check_move();
+            if (WINNER != ' ' || check_cell() == 0)
+            break;
+            print_board();
+            player_move2();
+            WINNER = check_move();
+            if (WINNER != ' ' || check_cell() == 0)
+            break;
+        }
+
+        print_board();
+        check_win(WINNER);
+        printf("\nNouvelle partie? (O/N): ");
+        scanf(" %c", &continuer);
+    }
+    return 0;
 }
