d60d8a155ef5efafc610ef073ed50836b4419beb
Saltyy <clement.bouret@epitech.eu>
2024-02-08 18:33:40 +0100

    [+] functions for victory check

---
+++ TicTacToe/src/check_all.c
@@ -0,0 +1,39 @@
+#include "../include/tictactoe.h"
+
+int check_cell()
+{
+    int free = 9; //free space in the board at the start of the game
+
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
+            if (board[i][j] != ' ')
+                free--;
+        }
+    }
+    return free;
+}
+
+int check_move()
+{
+    for (int i = 0; i < 3; i++){
+        if (board[0][i] == board[1][i] && board [1][i] == board [2][i])
+            return board[0][i];
+         if (board[i][0] == board[i][1] && board [i][1] == board [i][2])
+            return board[i][0];
+         if (board[0][0] == board[1][1] && board [1][1] == board [2][2])
+            return board[0][0];
+         if (board[0][2] == board[1][1] && board [1][1] == board [2][0])
+            return board[0][2];
+    }
+    return ' ';
+}
+
+void check_win(char WINNER)
+{
+    if (WINNER == JOUEUR)
+        printf("Joueur 1 a gagné");
+    else if (WINNER == JOUEUR2)
+        printf("Joueur 2 a gagné");
+    else 
+        printf("Match Nul");
+}
