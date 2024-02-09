d95e1e51164d594b85b24a56f70c7a9241498427
Saltyy <clement.bouret@epitech.eu>
2024-02-09 17:30:24 +0100

    [~] function for playing against bot or human

---
+++ TicTacToe/src/bot_or_human.c
@@ -0,0 +1,33 @@
+#include "../include/tictactoe.h"
+
+void play_against_bot()
+{
+    while (WINNER == ' ' && check_cell() != 0) {
+            print_board();
+            player_move();
+            WINNER = check_move();
+            if (WINNER != ' ' || check_cell() == 0)
+            break;
+            print_board();
+            bot_move();
+            WINNER = check_move();
+            if (WINNER != ' ' || check_cell() == 0)
+            break;
+        }
+}
+
+void play_against_human()
+{
+    while (WINNER == ' ' && check_cell() != 0) {
+            print_board();
+            player_move();
+            WINNER = check_move();
+            if (WINNER != ' ' || check_cell() == 0)
+            break;
+            print_board();
+            player_move2();
+            WINNER = check_move();
+            if (WINNER != ' ' || check_cell() == 0)
+            break;
+        }
+}
