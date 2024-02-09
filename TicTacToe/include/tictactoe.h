726d0508f1b034922fabc91c8cdafd4e8d7f5a41
Saltyy <clement.bouret@epitech.eu>
2024-02-09 15:50:35 +0100

    [~] addition of bot declaration

--- TicTacToe/include/tictactoe.h
+++ TicTacToe/include/tictactoe.h
@@ -7,6 +7,7 @@
 extern char board[3][3];
 extern const char JOUEUR;
 extern const char JOUEUR2;
+extern const char BOT;
 extern char WINNER;
 
 void player_move();
@@ -16,6 +17,7 @@ void print_board();
 int check_cell();
 int check_move();
 void check_win(char WINNER);
+void bot_move();
 
 
 #endif /*TICTACTOE*/
