18be02b8a473798387f03683a7cb5b9e1459454a
Saltyy <clement.bouret@epitech.eu>
2024-02-09 19:29:05 +0100

    [~] error handling were there was a segfault if the input was a char, now it only accepts num between 1 and 3

--- TicTacToe/src/players_move.c
+++ TicTacToe/src/players_move.c
@@ -3,45 +3,68 @@
 void player_move()
 {
     int x;
-    int y;
+    int y; 
+    int input;
 
-   do
-   {
-     printf("Entrez Ligne (1-3): ");
-    scanf("%d", &x);
-    x--;
-    printf("Entrez Colonne (1-3): ");
-    scanf("%d", &y);
-    y--;
+    do {
+        printf("Entrez Ligne (1-3): ");
+        input = scanf("%d", &x);
+        if(input != 1) {
+            while(getchar() != '\n');
+            printf("Veuillez entrer un chiffre entre 1 et 3.\n");
+            continue;
+        }
+        x--;
+        printf("Entrez Colonne (1-3): ");
+        input = scanf("%d", &y);
 
-    if (board[x][y] != ' '){
-        printf("Coup invalide\n");
-    } else{
-        board[x][y] = JOUEUR;
-        break;
-    }
-   } while (board[x][y] != ' ');
+        if(input != 1 || x < 0 || x > 2 || y < 1 || y > 3) {
+            while(getchar() != '\n');
+            printf("Veuillez entrer un chiffre entre 1 et 3.\n");
+            continue;
+        }
+
+        y--;
+
+        if (board[x][y] != ' ') {
+            printf("Coup invalide\n");
+        } else {
+            board[x][y] = JOUEUR;
+            break;
+        }
+    } while (1);
 }
+
 
 void player_move2()
 {
     int x;
-    int y;
+    int y; 
+    int input;
 
-   do
-   {
-     printf("Entrez Ligne (1-3): ");
-    scanf("%d", &x);
-    x--;
-    printf("Entrez Colonne (1-3): ");
-    scanf("%d", &y);
-    y--;
+    do {
+        printf("Entrez Ligne (1-3): ");
+        input = scanf("%d", &x);
+        if(input != 1) {
+            while(getchar() != '\n');
+            printf("Veuillez entrer un chiffre entre 1 et 3.\n");
+            continue;
+        }
+        x--;
+        printf("Entrez Colonne (1-3): ");
+        input = scanf("%d", &y);
 
-    if (board[x][y] != ' '){
-        printf("Coup invalide\n");
-    } else{
-        board[x][y] = JOUEUR2;
-        break;
-    }
-   } while (board[x][y] != ' ');
+        if(input != 1 || x < 0 || x > 2 || y < 1 || y > 3){
+            while(getchar() != '\n');
+            printf("Veuillez entrer un chiffre entre 1 et 3.\n");
+            continue;
+        }
+        y--;
+        if (board[x][y] != ' '){
+            printf("Coup invalide\n");
+        } else {
+            board[x][y] = JOUEUR2;
+            break;
+        }
+    } while (1);
 }
