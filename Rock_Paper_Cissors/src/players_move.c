756b746bbd6d7da0a647e88a8a03dff516584c81
Saltyy <clement.bouret@epitech.eu>
2024-02-29 16:29:09 +0100

    [+] player_move function squeletton for rock paper cissors game

---
+++ Rock_Paper_Cissors/src/players_move.c
@@ -0,0 +1,25 @@
+#include "../include/tictactoe.h"
+
+void player_move()
+{
+    int x;
+    int y; 
+    int input;
+
+    do {
+        printf("Entrez votre coup: ");
+        input = scanf("%d", &x);
+}
+
+
+void player_move2()
+{
+    int x;
+    int y; 
+    int input;
+
+    do {
+        printf("Entrez votre coup: ");
+        input = scanf("%d", &x);
+  
+}
