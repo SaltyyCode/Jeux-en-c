8302082afac0a7f9a7ab1e8746af63c419d6a6db
Clément // Saltyy <clementbouret59@gmail.com>
2024-03-17 14:57:58 +0100

    [+] added evaluation function for bot

--- TicTacToe/src/play_bot.c
+++ TicTacToe/src/play_bot.c
@@ -1,21 +1,28 @@
 #include "../include/tictactoe.h"
 
-void bot_move() // utiliser rand ou srand
+int my_eval (char board[3][3])
 {
-    int x;
-    int y;
-    srand(time(0));
-
-    if (check_cell() > 0){
-        do
-        {
-            x = rand() % 3;
-            y = rand() % 3;
-            if (board[x][y] != ' '){
-            }else {
-                board[x][y] = BOT;
-                break;
-            }
-        } while (board[x][y] != ' ');
+    for (int r = 0; r < 3; r++){
+        if (board[r][0] == board[r][1] && board[r][1] == board[r][2]){
+            if (board[r][0] == BOT)
+                return +10;
+            else if (board[r][0] == JOUEUR)
+                return -10;
+        }
     }
+    for (int c = 0; c < 3; c++){
+        if (board[0][c] == board[1][c] && board[1][c] == board[2][c]){
+            if (board[0][c] == BOT)
+                return +10;
+            else if (board[0][c] == JOUEUR)
+                return -10;
+        }
+    }
+    if (board[0][0] == board[1][1] && board[1][1] == board[2][2] || board[0][2] == board[1][1] && board[1][1] == board[2][0]){
+        if (board[1][1] == BOT)
+            return +10;
+        else if (board[1][1] == JOUEUR)
+            return -10;
+    }
+    return 0;
 }
